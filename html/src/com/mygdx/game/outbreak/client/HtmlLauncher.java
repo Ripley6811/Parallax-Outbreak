2826ed42c229ab6c52cb00b400c936d7bef172fe
Jay W Johnson <jwj@boun.cr>
2016-01-12 00:03:53 +0800

    Adjusted HTML dist application size

--- html/src/com/mygdx/game/outbreak/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/outbreak/client/HtmlLauncher.java
@@ -3,13 +3,17 @@ package com.mygdx.game.outbreak.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.outbreak.Constants;
 import com.mygdx.game.outbreak.OutbreakGame;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(
+                        Constants.TEXT_VIEWPORT_SIZE[0],
+                        Constants.TEXT_VIEWPORT_SIZE[1]
+                );
         }
 
         @Override
